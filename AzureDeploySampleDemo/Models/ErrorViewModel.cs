32ab9d528b5f725703fdf6693ed81ba6eae34df8
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-11-21 12:43:01 +0100

    sample commit

---
+++ AzureDeploySampleDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureDeploySampleDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
